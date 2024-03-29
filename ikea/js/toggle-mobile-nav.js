2891bb32d1715f86c006b279f341f23a0a153b88
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-03-29 18:27:06 +0100

    aggiunta mobile-navbar (wip) e file js con lo script per aprirla e chiuderla

---
+++ ikea/js/toggle-mobile-nav.js
@@ -0,0 +1,13 @@
+const mobileNavOpenBtn = document.querySelector(".mobile-ico")
+const mobileNav = document.querySelector(".mobile-nav")
+const mobileNavCloseBtn = document.querySelector (".mobile-nav .close")
+
+mobileNavCloseBtn.addEventListener("click", () => {
+    mobileNav.classList.toggle("non-visible")    
+    mobileNav.classList.toggle("visible")
+})
+
+mobileNavOpenBtn.addEventListener("click", () => {
+    mobileNav.classList.toggle("visible")
+    mobileNav.classList.toggle("non-visible")
+})
