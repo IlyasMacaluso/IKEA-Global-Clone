c72d329b9661f309e8678be231800c848bcdc873
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-04-12 18:32:41 +0200

    Update animazioni scroll
    -modificati i tempi dell'animazione
    -modificato il css per attivare l'animazione su tutti i tre tipi di pox differenti
    -modificato il css per rimuovere le barredi scorrimento
    -modificato il js per attivare l'animazione su utti i tre tipi di box dirfferenti

--- ikea/js/sliding-boxes.js
+++ ikea/js/sliding-boxes.js
@@ -1,11 +1,26 @@
-document.addEventListener('scroll', () => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {
-        // const triggerBottom = ; // Trigger at 80% of the viewport
-        if (box.getBoundingClientRect().top < window.innerHeight) {
-            box.style.visibility = 'visible';
-            box.style.animationPlayState = 'running';
-        }
+const boxes = document.querySelectorAll(".box")
+const boxesFancy = document.querySelectorAll(".box-fancy")
+const carousels = document.querySelectorAll (".carousel")
 
+document.addEventListener("scroll", () => {
+    boxes.forEach((box) => {
+        // const triggerBottom = ; // Trigger at 80% of the viewport
+        if (box.getBoundingClientRect().top < window.innerHeight + 200) {
+            box.style.visibility = "visible"
+            box.style.animationPlayState = "running"
+        }
+    })
+    boxesFancy.forEach((boxFancy) => {
+        // const triggerBottom = ; // Trigger at 80% of the viewport
+        if (boxFancy.getBoundingClientRect().top < window.innerHeight + 200 ) {
+            boxFancy.style.visibility = "visible"
+            boxFancy.style.animationPlayState = "running"
+        } 
+    })
+    carousels.forEach(carousel => {
+        if (carousel.getBoundingClientRect().top < window.innerHeight + 200 ) {
+            carousel.style.visibility = "visible"
+            carousel.style.animationPlayState = "running"
+        }
     });
-});
+})
