8403674540fa50705d04f7a59d37839b174dbe53
IlyasMacaluso <ilyas.macaluso@gmail.com>
2024-04-13 15:28:42 +0200

    Sliding boxes improvements
    -Migliorate le animazioni
    -Creata una funzione riutilizzabile per ridurre le righe di codice necessarie per le animazioni
    -Aggiunto un eventListener che anima e rende visibili le box che rispettano la condizione di visibilità al caricamento della pagina

--- ikea/js/sliding-boxes.js
+++ ikea/js/sliding-boxes.js
@@ -1,34 +1,35 @@
 const boxes = document.querySelectorAll(".box")
-const boxesHero = document.querySelectorAll (".box-hero")
+const boxesHero = document.querySelectorAll(".box-hero")
 const boxesFancy = document.querySelectorAll(".box-fancy")
-const carousels = document.querySelectorAll (".carousel")
+const carousels = document.querySelectorAll(".carousel")
 
-boxesHero.forEach(boxHero => {
+boxesHero.forEach((boxHero) => {
     boxHero.style.visibility = "visible"
-
     boxHero.style.animationPlayState = "running"
 })
 
-document.addEventListener("scroll", () => {
-    boxes.forEach((box) => {
-        if(box.classList.contains("hero")){}
-        // const triggerBottom = ; // Trigger at 80% of the viewport
-        if (box.getBoundingClientRect().top < window.innerHeight + 200) {
-            box.style.visibility = "visible"
-            box.style.animationPlayState = "running"
+/* funzione che anima tutti gli elementi all'interno di una nodeList che passiamo come parametro */
+function slideAnimation(node) {
+    node.forEach((element) => {
+        const rectY = element.getBoundingClientRect().top
+        if (rectY < window.innerHeight * 1.2) {
+            element.style.visibility = "visible"
+            element.style.animationPlayState = "running"
+        } else {
+            element.style.visibility = "hidden"
+            element.style.animationPlayState = "paused"
         }
     })
-    boxesFancy.forEach((boxFancy) => {
-        // const triggerBottom = ; // Trigger at 80% of the viewport
-        if (boxFancy.getBoundingClientRect().top < window.innerHeight + 200 ) {
-            boxFancy.style.visibility = "visible"
-            boxFancy.style.animationPlayState = "running"
-        } 
-    })
-    carousels.forEach(carousel => {
-        if (carousel.getBoundingClientRect().top < window.innerHeight + 200 ) {
-            carousel.style.visibility = "visible"
-            carousel.style.animationPlayState = "running"
-        }
-    })
+}
+
+window.addEventListener("load", () => {
+    slideAnimation(boxes)
+    slideAnimation(boxesFancy)
+    slideAnimation(carousels)
 })
+document.addEventListener("scroll", () => {
+    slideAnimation(boxes)
+    slideAnimation(boxesFancy)
+    slideAnimation(carousels)
+})
+
